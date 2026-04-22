0d1cee7c8d0174e5c0f3ea4de9fa0068eacbc45e
hensim25 <hensim25@kingshigh.school.nz>
2026-04-22 12:24:20 +1200

    V1.2
    added animations to player

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -23,6 +23,17 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	
+	
 	
 	if direction:
 		velocity.x = direction * SPEED
